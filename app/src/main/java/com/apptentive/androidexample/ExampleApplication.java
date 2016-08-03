5694e514f9633684d64eeef0feeb0232c7affaf2
skykelsey <sky.kelsey@gmail.com>
2016-08-02 19:06:16 -0700

    Override the Application class.

---
+++ app/src/main/java/com/apptentive/androidexample/ExampleApplication.java
@@ -0,0 +1,11 @@
+package com.apptentive.androidexample;
+
+import android.app.Application;
+
+
+public class ExampleApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
