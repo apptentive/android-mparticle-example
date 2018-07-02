893ba6a16b6d0d09a45db2e92e884a9323e2e11d
Alex Lementuev <a.lementuev@gmail.com>
2018-07-02 14:04:14 -0700

    Migrated to the latest mParticle version

--- app/src/androidTest/java/mparticle/androidexample/apptentive/com/android_mparticle_example/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/mparticle/androidexample/apptentive/com/android_mparticle_example/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.apptentive.androidexample;
+package com.apptentive.androidexample.mparticle;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -10,17 +10,17 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
- * Instrumentation test, which will execute on an Android device.
+ * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-	@Test
-	public void useAppContext() throws Exception {
-		// Context of the app under test.
-		Context appContext = InstrumentationRegistry.getTargetContext();
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
 
-		assertEquals("com.apptentive.androidexample", appContext.getPackageName());
-	}
+        assertEquals("com.apptentive.androidexample.mparticle", appContext.getPackageName());
+    }
 }
